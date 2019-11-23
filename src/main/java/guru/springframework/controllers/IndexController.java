47fabb07e8b41d6621328634afbf518d98bcdaa3
rsgillit <rsgillit@gmail.com>
2019-11-23 10:48:44 -0600

    Merge branch 'one-to-one-jpa-assign-review' of https://github.com/rsgillit/spring5-recipe-app into one-to-one-jpa-assign-review
    # Conflicts:
    #	src/main/java/guru/springframework/controllers/IndexController.java
    #	src/main/java/guru/springframework/domain/Ingredient.java
    #	src/main/java/guru/springframework/domain/Notes.java
    #	src/main/java/guru/springframework/domain/Recipe.java
    #	src/main/java/guru/springframework/domain/UnitOfMeasure.java

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,14 +1,8 @@
 package guru.springframework.controllers;
 
-<<<<<<< HEAD
-public class IndexController {
-=======
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by jt on 6/1/17.
- */
 @Controller
 public class IndexController {
 
@@ -16,5 +10,4 @@ public class IndexController {
     public String getIndexPage(){
         return "index";
     }
->>>>>>> 974ec3fffeac8907bde5b6f21f30ef6a699d5694
 }
